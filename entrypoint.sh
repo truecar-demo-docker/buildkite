8606dac434448d59a63bafe0136f0b7cf462c15f
Ryan Long <rlong@truecar.com>
2019-05-01 19:41:06 -0700

    tweak build

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,12 +14,22 @@ fi
     export BUILDKITE_AGENT_TAGS="instance_id=${EC2_INSTANCE_ID},${BUILDKITE_AGENT_TAGS:-}"
 }
 
-[[ -n ${EC2_PRIVATE_IP:-} ]] && export BUILDKITE_AGENT_TAGS="ip_address=${EC2_PRIVATE_IP},${BUILDKITE_AGENT_TAGS:-}"
+[[ ${EC2_PRIVATE_IP:-} ]] && export BUILDKITE_AGENT_TAGS="ip_address=${EC2_PRIVATE_IP},${BUILDKITE_AGENT_TAGS:-}"
 
-[[ -n ${CONFIG_SHA:-} ]] && export BUILDKITE_AGENT_TAGS="config_sha=${CONFIG_SHA},${BUILDKITE_AGENT_TAGS:-}"
+[[ ${CONFIG_SHA:-} ]] && export BUILDKITE_AGENT_TAGS="config_sha=${CONFIG_SHA},${BUILDKITE_AGENT_TAGS:-}"
 
 container_id="$(awk -F/ '/1:name=systemd/ {print $NF}' /proc/self/cgroup)"
-this_image_id="$(docker inspect "${container_id:?}" | jq -r '.[0].Image')"
-[[ -n ${this_image_id:-} ]] && export BUILDKITE_AGENT_TAGS="agent_docker_image=${this_image_id},${BUILDKITE_AGENT_TAGS:-}"
+[[ ${container_id:-} ]] && this_image_id="$(docker inspect "${container_id:?}" | jq -r '.[0].Image')"
+[[ ${this_image_id:-} ]] && export BUILDKITE_AGENT_TAGS="agent_docker_image=${this_image_id},${BUILDKITE_AGENT_TAGS:-}"
+
+
+if [[ -n ${REGISTRY_HOST:-} ]]; then
+    conf_path="$HOME/.docker/config.json"
+    conf='{}'
+    if [[ -f ${conf_path} ]]; then
+        conf="$(< "$conf_path")"
+    fi
+    echo "${conf}" | jq --arg host "${REGISTRY_HOST}" '. + { credHelpers: ((.credHelpers // {}) + { ($host): "ecr-login" }) } | if .credsStore == "ecr-login" then del(.credsStore) else . end' > "${conf_path}"
+fi
 
 exec "$@"
