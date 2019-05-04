ab7d6091646dc773ebd21097daee023f320d6d44
Ryan Long <rlong@truecar.com>
2019-05-03 18:41:54 -0700

    tweak build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,52 @@
 #!/bin/bash
 
 set -euo pipefail
-[[ ${BUILDKITE_AGENT_DEBUG:-false} = true ]] && set -x
+[[ ${BUILDKITE_AGENT_DEBUG:-false} = true ]] && set -xv
 
-if [[ -f /run/ec2-metadata ]]; then
-    set -o allexport
-    source /run/ec2-metadata
-    set +o allexport
-fi
-
-[[ -n ${EC2_INSTANCE_ID:-} ]] && {
-    export BUILDKITE_AGENT_NAME="${EC2_INSTANCE_ID}-%hostname"
-    export BUILDKITE_AGENT_TAGS="instance_id=${EC2_INSTANCE_ID},${BUILDKITE_AGENT_TAGS:-}"
+function copy_binary() {
+    # /buildkite/bin is mounted as a host volume to make the binary available for
+    # builds to share with their constituent containers. because it's a host volume,
+    # it may end up empty or containing an old binary, so copy our binary to it in
+    # that case. # FIXME: this doesn't seem terribly robust, but it works for now.
+    local binary_path="$(which buildkite-agent)"
+    [[ -x /buildkite/bin/buildkite-agent ]] && diff -q "$binary_path" /buildkite/bin/buildkite-agent && return 0
+    cp -v "$binary_path" /buildkite/bin/
 }
 
-[[ ${EC2_PRIVATE_IP:-} ]] && export BUILDKITE_AGENT_TAGS="ip_address=${EC2_PRIVATE_IP},${BUILDKITE_AGENT_TAGS:-}"
+function container_id() {
+    awk -F/ '/:name=systemd/ {print $NF}' /proc/self/cgroup
+}
 
-[[ ${CONFIG_SHA:-} ]] && export BUILDKITE_AGENT_TAGS="config_sha=${CONFIG_SHA},${BUILDKITE_AGENT_TAGS:-}"
-
-container_id="$(awk -F/ '/1:name=systemd/ {print $NF}' /proc/self/cgroup)"
-[[ ${container_id:-} ]] && this_image_id="$(docker inspect "${container_id:?}" | jq -r '.[0].Image')"
-[[ ${this_image_id:-} ]] && export BUILDKITE_AGENT_TAGS="agent_docker_image=${this_image_id},${BUILDKITE_AGENT_TAGS:-}"
-
-
-if [[ -n ${REGISTRY_HOST:-} ]]; then
-    conf_path="$HOME/.docker/config.json"
-    conf='{}'
-    if [[ -f ${conf_path} ]]; then
-        conf="$(< "$conf_path")"
+function configure_environment() {
+    if [[ -f /run/ec2-metadata ]]; then
+        set -o allexport # this file is lines of `VAR=value` so use allexport to make those vars exported automatically
+        source /run/ec2-metadata
+        set +o allexport
     fi
+
+    [[ -n ${EC2_INSTANCE_ID:-} ]] && {
+        export BUILDKITE_AGENT_NAME="${EC2_INSTANCE_ID}-%hostname"
+        export BUILDKITE_AGENT_TAGS="instance_id=${EC2_INSTANCE_ID},${BUILDKITE_AGENT_TAGS:-}"
+    }
+
+    [[ ${EC2_PRIVATE_IP:-} ]] && export BUILDKITE_AGENT_TAGS="ip_address=${EC2_PRIVATE_IP},${BUILDKITE_AGENT_TAGS:-}"
+
+    local this_image_id="$(docker inspect "$(container_id)" | jq -r '.[0].Image')"
+    [[ ${this_image_id:-} ]] && export BUILDKITE_AGENT_TAGS="agent_docker_image=${this_image_id},${BUILDKITE_AGENT_TAGS:-}"
+}
+
+function configure_docker() {
+    # if credsStore=ecr-login we'll see warnings when pulling from any non-ECR
+    # repo so this refines the config to just use ecr-login for our one REGISTRY_HOST
+
+    [[ ${REGISTRY_HOST:-} ]] || return 0
+    local conf_path="$HOME/.docker/config.json"
+    local conf='{}'
+    [[ -f ${conf_path} ]] && conf="$(< "$conf_path")"
     echo "${conf}" | jq --arg host "${REGISTRY_HOST}" '. + { credHelpers: ((.credHelpers // {}) + { ($host): "ecr-login" }) } | if .credsStore == "ecr-login" then del(.credsStore) else . end' > "${conf_path}"
-fi
+}
 
-# /buildkite/bin is mounted as a host volume to make the binary available for
-# builds to share with their constituent containers. because it's a host volume,
-# it may end up empty or containing an old binary, so copy our binary to it in
-# that case. # FIXME: this doesn't seem terribly robust, but it works for now.
-[[ -x /buildkite/bin/buildkite-agent ]] && diff -q /usr/local/bin/buildkite-agent /buildkite/bin/buildkite-agent || cp -v /usr/local/bin/buildkite-agent /buildkite/bin/
-
+configure_environment
+configure_docker
+copy_binary
 exec "$@"
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 set -x
 
-test -x /buildkite/bin/bootstrap-via-docker
+test -x /buildkite/bootstrap-via-docker
 # ensure /buildkite volume has captured the binary linked into the /buildkite/bin dir before volume's creation
 /buildkite/bin/buildkite-agent --version
 # ensure the rest of these tools are at least runnable
