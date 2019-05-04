6930dda58cdb380db4a431a0f2d329c26b62eb40
Ryan Long <rlong@truecar.com>
2019-05-03 17:33:22 -0700

    Ensure binary always gets copied to /buildkite/bin
    since sometimes that path is a host volume

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,4 +32,10 @@ if [[ -n ${REGISTRY_HOST:-} ]]; then
     echo "${conf}" | jq --arg host "${REGISTRY_HOST}" '. + { credHelpers: ((.credHelpers // {}) + { ($host): "ecr-login" }) } | if .credsStore == "ecr-login" then del(.credsStore) else . end' > "${conf_path}"
 fi
 
+# /buildkite/bin is mounted as a host volume to make the binary available for
+# builds to share with their constituent containers. because it's a host volume,
+# it may end up empty or containing an old binary, so copy our binary to it in
+# that case. # FIXME: this doesn't seem terribly robust, but it works for now.
+[[ -x /buildkite/bin/buildkite-agent ]] && diff -q /usr/local/bin/buildkite-agent /buildkite/bin/buildkite-agent || cp -v /usr/local/bin/buildkite-agent /buildkite/bin/
+
 exec "$@"
