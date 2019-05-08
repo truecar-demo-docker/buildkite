5b8a390ed7c476450ea6a922d3d22e5925ac3561
Ryan Long <rlong@truecar.com>
2019-05-08 15:26:19 -0700

    Add support for resolving environment values from buildkite meta-data

--- buildkite/hooks/environment.py
+++ buildkite/hooks/environment.py
@@ -5,23 +5,65 @@
 import os
 import sys
 import re
+import subprocess
 
 import boto3
+from botocore.exceptions import ClientError, ParamValidationError
 
-ssm_client = boto3.client('ssm')
+ssm = boto3.client('ssm')
 
-ssm_param_pattern = re.compile('^ssm-parameter:([a-zA-Z0-9_./-]+)$')
+ssm_param_pattern = re.compile('^ssm-parameter:(.+)$')
+metadata_param_pattern = re.compile('^buildkite-meta-data:(.+)$')
+
+
+def buildkite_metadata_get(var, key):
+    try:
+        command = ['buildkite-agent', 'meta-data', 'get', key]
+        return subprocess.check_output(command).decode().rstrip('\n')
+    except subprocess.CalledProcessError as e:
+        print(f'ERROR while attempting to resolve ${var} using buildkite '
+              f'meta-data {key}: exit={e.returncode}',
+              file=sys.stderr)
+
+
+def resolve_ssm_var(var, param_path):
+    try:
+        resp = ssm.get_parameter(Name=param_path, WithDecryption=True)
+        return resp['Parameter']['Value']
+    except ssm.exceptions.ParameterNotFound:
+        print(f'ERROR while resolving var {var} using SSM parameter {key}: '
+              'ParameterNotFound', file=sys.stderr)
+    except ParamValidationError as e:
+        print("ERROR boto3.ParamValidationError while resolving var {var} "
+              f"using SSM parameter {key}: {e}", file=sys.stderr)
+    except ClientError as e:
+        print(f'ERROR boto3.ClientError while resolving var {var} using SSM '
+              f'parameter {key}: {e}', file=sys.stderr)
+
+
+def export_var(var, value):
+    if value:
+        print(f"export {var}='{value}'")
+    else:
+        print(f"WARNING: Variable {var} was resolved to None (likely an error "
+              "occurred); variable will NOT be present in build.",
+              file=sys.stderr)
+        print(f'unset {var}')
+
 
 for var, value in os.environ.items():
     match = ssm_param_pattern.match(value)
-    if match is None:
+    if match is not None:
+        key = match.group(1)
+        value = resolve_ssm_var(key)
+        export_var(var, value)
         continue
-    param_path = match.group(1)
-    resp = ssm_client.get_parameter(Name=param_path, WithDecryption=True)
 
-    print(f'Resolved ENV {var} value from SSM parameter {param_path}',
-          file=sys.stderr)
+    match = metadata_param_pattern.match(value)
+    if match is not None:
+        key = match.group(1)
+        value = buildkite_metadata_get(key)
+        export_var(var, value)
+        continue
 
-    value = resp['Parameter']['Value']
-
-    print(f"export {var}='{value}'")
+    # else: no-op, don't export an overriden value for this var
